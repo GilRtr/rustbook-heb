ea360d03ee768fd7dd774cb330c4b68fab4b88dc
Gil Rtr <glrtr2003@gmail.com>
2021-02-06 00:18:26 +0200

    Deleted files that are irrelevant to the translation

--- ci/validate.sh
+++
@@ -1,4 +0,0 @@
-for file in src/*.md ; do
-    echo Checking references in $file
-    cargo run --quiet --bin link2print < $file > /dev/null
-done
--- listings/ch06-enums-and-pattern-matching/no-listing-12-if-let/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // ANCHOR: here
-    let some_u8_value = Some(0u8);
-    if let Some(3) = some_u8_value {
-        println!("three");
-    }
-    // ANCHOR_END: here
-}
